ca3f58fafa7409bb3f38546794c34f0dd350f78a
Kristen Chellis and Matt Nardoni <kristen@>
2018-03-22 11:55:59 -0700

    Add javascript branches for determining match add class to option divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,31 @@
 $(function() {
-  $("#formOne").submit(function(event) {
+  $("form#formOne").submit(function(event) {
     event.preventDefault();
-    var age = parseInt(("#age").val());
-    var gender = ("#gender").val();
-
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
     if (age) {
-
-      if (age > 70 && gender === "male") {
-
-      } else if (age > 70 && gender === "female") {
-
+      $("#young").text("");
+      $(".celebs").hide();
+      if (age >= 70 && gender === 'male') {
+        $("#trump").show();
+      } else if (age >= 70 && gender === "female") {
+        $("#turner").show();
       } else if (age >=55 && gender === "male") {
-
+        $("#mrt").show();
       } else if (age >=55 && gender === "female") {
-
+        $("#fey").show();
       } else if (age >=35 && gender === "male") {
-
+        $("#ice").show();
       } else if (age >=35 && gender === "female") {
-
+        $("#tomei").show();
       } else if (age >=18 && gender === "male") {
-
+        $("#lautner").show();
       } else if (age >=18 && gender === "female") {
-
+        $("#swift").show();
       } else {
-
+        $("#young").text("Get Out!");
       }
-
-    } else {
+  } else {
       alert('Please enter your age.');
     }
 
